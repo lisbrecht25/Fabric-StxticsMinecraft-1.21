bd9e8bc3377af6adf6a4ff6f2a6fcc7ab16d66f5
lisbrecht25 <lukeisbrecht25@gmail.com>
2025-05-21 11:08:49 -0400

    Initial commit

---
+++ src/main/java/net/stxtic/stxticsminecraft/StxticsMinecraft.java
@@ -0,0 +1,16 @@
+package net.stxtic.stxticsminecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StxticsMinecraft implements ModInitializer {
+	public static final String MOD_ID = "stxtics-minecraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/stxtic/stxticsminecraft/StxticsMinecraftClient.java
@@ -0,0 +1,10 @@
+package net.stxtic.stxticsminecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StxticsMinecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
